ea14e372285404091a5c8458c5a5edf51e15d7d1
Rohit Kumar <rohit2004r82@gmail.com>
2025-09-15 16:27:52 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "https://yardstick-internship-assignment-fro.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: true, 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
