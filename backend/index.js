30e997740374b06a3da5a87ed666aee90d223192
Rohit Kumar <rohit2004r82@gmail.com>
2025-09-15 15:33:13 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const connectDB = require("./config/connectDB");
-// const cookieParser = require("cookie-parser");
-// const authRoutes = require("./routes/authRoutes");
-// const noteRoutes = require("./routes/noteRoutes");
-// const tenantRoutes = require("./routes/tenantRoutes");
-// require("dotenv").config();
-
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-
-// app.use(cors({
-//   origin: true, 
-//   credentials: true, 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use("/api/auth", authRoutes)
-// app.use("/api/notes", noteRoutes)
-// app.use("/api/tenants", tenantRoutes)
-
-// app.get("/health", (req, res) => {
-//   res.status(200).json({ status: "ok" });
-// });
-
-
-
-// const startServer = async () => {
-//   try {
-//     await connectDB();
-//     app.listen(port, () => {
-//       console.log(`Server running on port ${port}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// startServer();
-
 
 const express = require("express");
 const cors = require("cors");
@@ -57,7 +12,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "https://yardstick-internship-assignment-fro.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
